0284a1dde70f7280b7b8cef1db68aac64050c91e
Esemega <sogaral89@gmail.com>
2020-09-10 19:49:11 +0200

    Adding Project card component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Me from './components/Me/Me';
 import Contact from './components/Contact/Contact.js';
 import NavBar from './components/NavBar/NavBar.js';
+import Project from './components/Project/Project.js';
 
 import './App.css';
 
@@ -10,6 +11,13 @@ function App() {
     <div className="App">
       <NavBar />
       <Me />
+      <Project 
+      image='../../../img/robofriends.jpg' 
+      name='Robofriends' 
+      description='Simple App to start with APIs.'
+      code='https://github.com/Esemega/robofriends'
+      live='https://esemega.github.io/robofriends/'
+      />
       <Contact />
     </div>
   );
